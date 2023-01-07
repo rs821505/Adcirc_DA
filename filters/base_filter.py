aa3544541203d6f7b9e967ad6f26f0cdae985220
rs821505 <107643011+rs821505@users.noreply.github.com>
2023-01-07 16:15:20 -0600

    added comments to base_filter, renamed mean

--- filters/base_filter.py
+++ filters/base_filter.py
@@ -8,6 +8,7 @@ import numpy as np
 
 @dataclass(frozen=True, order=True)
 class base_filter:
+    """Base filter class for ensemble kalman filter algorithm"""
 
     state_forecast: np.ndarray = None
     model_observations: np.ndarray = None
@@ -16,16 +17,26 @@ class base_filter:
     inf_fact: float = 1.65
 
     def get_shape(self):
+        """Get shapes for data assimilation
+
+        Returns
+        ---------
+        int
+            ny: observation vector dimension
+            nx: state forecast vector dimensions
+            ne: number of members in ensemble
+        """
         self.ny = np.size(self.observations)
         self.nx, self.ne = self.state_forecast.shape
 
     def get_means(self):
+        """Assigns state ensemble mean, observation mean, and the centered state and observation matrices"""
 
         self.state_mean = self.state_forecast.mean(axis=1)
         self.observation_mean = self.model_observations.mean(axis=1)
 
-        self.zero_mean_state = self.state_forecast - self.state_mean[:None]
-        self.zero_mean_observations = self.observations - np.expand_dims(
+        self.centered_state_forecasts = self.state_forecast - self.state_mean[:None]
+        self.centered_observations = self.observations - np.expand_dims(
             self.observation_mean, axis=1
         )
 
